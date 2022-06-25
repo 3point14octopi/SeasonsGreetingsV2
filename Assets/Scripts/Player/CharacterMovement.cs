cc751cf4cac9e56b65d1d3ba26d01adfbdb1f80b
Isaac Taylor <isaac.taylor@ontariotechu.net>
2022-06-25 15:40:36 -0400

    Character has blended movement anims

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -6,10 +6,16 @@ public class CharacterMovement : MonoBehaviour
 {
 
     public float speed;
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
         transform.position = transform.position + speed * movement * Time.deltaTime;
     }
 }
